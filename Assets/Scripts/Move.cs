ec03f2879216d058a6875c0691e5851bd0391548
Kristian Sakarisson <kristian@sakarisson.com>
2017-01-21 02:47:15 +0000

    Added player and various scripts for player

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    private bool jumping = false;
+	void Update ()
+    {
+        Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        gameObject.GetComponent<Rigidbody>().velocity = moveDir * 3 + new Vector3(0f, GetComponent<Rigidbody>().velocity.y, 0f);
+        Debug.Log(moveDir.magnitude);
+        if (moveDir.magnitude > .05f)
+        {
+            gameObject.transform.rotation = Quaternion.LookRotation(moveDir, Vector3.up);
+            if (!jumping)
+            {
+                StartCoroutine("jump");
+            }
+        }
+    }
+
+    IEnumerator jump()
+    {
+        jumping = true;
+        gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 15, ForceMode.Impulse);
+        yield return new WaitForSeconds(.5f);
+        jumping = false;
+    }
+}
---
+++ Assets/Scripts/endZone.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endZone : MonoBehaviour {
+
+    private List<GameObject> allSheep = new List<GameObject>();
+    private List<GameObject> sheepInsideZone = new List<GameObject>();
+
+    void Start ()
+    {
+        foreach(GameObject sheep in GameObject.FindGameObjectsWithTag("sheep"))
+        {
+            allSheep.Add(sheep);
+        }
+	}
+
+    private bool allSheepInZone()
+    {
+        foreach (GameObject sheep in allSheep)
+        {
+            if (!sheepInsideZone.Contains(sheep))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "sheep" && !sheepInsideZone.Contains(other.gameObject))
+        {
+            sheepInsideZone.Add(other.gameObject);
+            Debug.Log("Sheep entered!");
+            Debug.Log(allSheepInZone());
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "sheep" && sheepInsideZone.Contains(other.gameObject))
+        {
+            sheepInsideZone.Remove(other.gameObject);
+            Debug.Log("Sheep left!");
+        }
+    }
+}
